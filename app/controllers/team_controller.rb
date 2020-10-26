ff9f70ec264fb8e3efdf32e0e163d9f4f20d33ff
artdecole <nimrod@gmx.fr>
2020-10-27 00:41:59 +0100

    v

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,19 @@
+class TeamController < ApplicationController 
+    def presentation
+        puts "Salut"
+    end 
+    def contact
+    end 
+
+    def home
+        @list_gossips = Gossip.all
+      end
+
+ def gossip
+    @gossip_searched = Gossip.find(params[:gossip_number]) 
+  end
+
+  def author
+    @author_searched = User.find(params[:author_info]) 
+  end
+end 
